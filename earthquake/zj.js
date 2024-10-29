f3b67ea3f40ed74c3c4cde588b8349cb609fb768
soosoh <hydrodddd@gmail.com>
2024-10-29 17:43:34 +0900

    z image

--- earthquake/zj.js
+++ earthquake/zj.js
@@ -4,15 +4,19 @@ guidelines
 key : 숫자로 이루어진 고유문자열, 안내사항 데이터에서 어떤 안내사항을 찾아 표시할지 결정하는 index역할
 
 sel : 2개의 선택지중 하나를 선택(t1 또는 t2)
-t1 선택시 key에 '1' append
-t2 선택시 key에 '2' append
+t1에 해당하는 버튼 선택시 key에 '1' append
+t2에 해당하는 버튼 선택시 key에 '2' append
 
 guide : 안내사항을 확인하고 '다음'을 선택
 '다음' 선택시 key에 '0' append
 
+end : 안내 종료
+
 head : 내용
 
-type : sel 또는 guide 선택
+type : sel 또는 guide 또는 end 선택
+
+img : 첨부 자료(없다면 none입력) 
 
 "" : 첫번째 sel/guide
 */
@@ -39,12 +43,12 @@ const d = {
   "110": {
     head: "흔들림이 멈출 때까지 구조물을 잡고 버티세요",
     type: "guide",
-    img: "1.jpg"
+    img: "none"
   },
   "1100": {
     head: "가스와 전기를 차단하고 문을 열어 출구를 확보하세요",
     type: "guide",
-    img: "2.jpg"
+    img: "2.png"
   },
   "11000": {
     head: "밖으로 나갈 때는 신발을 신고 이동하세요",
@@ -59,7 +63,7 @@ const d = {
   "1100000": {
     head: "급한 경사지를 피해 평탄한 곳으로 대피하세요",
     type: "guide",
-    img: "5.jpg"
+    img: "6.jpg"
   },
   "11000000": {
     type: "end",
@@ -87,17 +91,22 @@ const d = {
   "120000": {
     head: "급한 경사지를 피해 평탄한 곳으로 대피하세요",
     type: "guide",
-    img: "5.jpg"
+    img: "6.jpg"
   },
   "1200000": {
     type: "end",
   },
-  "2": {
-    head: "급한 경사지를 피해 평탄한 곳으로 대피하세요",
+  "2":{
+    head: "낙하물에 주의하세요",
     type: "guide",
     img: "5.jpg"
   },
   "20": {
+    head: "급한 경사지를 피해 평탄한 곳으로 대피하세요",
+    type: "guide",
+    img: "6.jpg"
+  },
+  "200": {
    type: "end",
   },
 };
